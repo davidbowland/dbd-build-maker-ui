3c515e8a82a974aeb8cc7976234ae757279eddc9
David Bowland <dbowland1@gmail.com>
2022-08-21 19:08:14 -0500

    Refactor build-create

--- src/components/build-create/create-card.tsx
+++ src/components/build-create/create-card.tsx
@@ -89,6 +89,28 @@ const CreateCard = ({ buildId, buildOptions, buildTokenResponse, channelId }: Cr
     })
   }
 
+  const renderOptionList = (title: string, name: string, value: string, options: string[]): JSX.Element => {
+    return (
+      <FormControl fullWidth>
+        <InputLabel id={`${name}-label`}>{title}</InputLabel>
+        <NativeSelect
+          aria-labelledby={`${name}-label`}
+          disabled={isSubmitting}
+          id={name}
+          inputProps={{ 'data-testid': name } as Partial<NativeSelectInputProps>}
+          onChange={(event) => setBuild({ ...build, [name]: event.target.value })}
+          value={value}
+        >
+          {options.map((value, index) => (
+            <option key={index} value={value}>
+              {value}
+            </option>
+          ))}
+        </NativeSelect>
+      </FormControl>
+    )
+  }
+
   const snackbarErrorClose = (): void => {
     setErrorMessage(undefined)
   }
@@ -142,168 +164,21 @@ const CreateCard = ({ buildId, buildOptions, buildTokenResponse, channelId }: Cr
               </RadioGroup>
             </FormControl>
             <Divider />
-            <FormControl fullWidth>
-              <InputLabel id="character-label">Character</InputLabel>
-              <NativeSelect
-                aria-labelledby="character-label"
-                disabled={isSubmitting}
-                id="character"
-                inputProps={{ 'data-testid': 'character' } as Partial<NativeSelectInputProps>}
-                onChange={(event) => setBuild({ ...build, character: event.target.value })}
-                value={build.character}
-              >
-                {characters.map((name, index) => (
-                  <option key={index} value={name}>
-                    {name}
-                  </option>
-                ))}
-              </NativeSelect>
-            </FormControl>
+            {renderOptionList('Character', 'character', build.character, characters)}
             <Divider />
-            {buildType === 'survivor' && (
-              <FormControl fullWidth>
-                <InputLabel id="item-label">Item</InputLabel>
-                <NativeSelect
-                  aria-labelledby="item-label"
-                  disabled={isSubmitting}
-                  id="item"
-                  inputProps={{ 'data-testid': 'item' } as Partial<NativeSelectInputProps>}
-                  onChange={(event) => setBuild({ ...build, item: event.target.value })}
-                  value={build.item}
-                >
-                  {Object.keys(buildOptions.SurvivorItems).map((name, index) => (
-                    <option key={index} value={name}>
-                      {name}
-                    </option>
-                  ))}
-                </NativeSelect>
-              </FormControl>
-            )}
-            {(buildType === 'killer' || build.item !== 'None') && (
-              <FormControl fullWidth>
-                <InputLabel id="addon1-label">Addon 1</InputLabel>
-                <NativeSelect
-                  aria-labelledby="addon1-label"
-                  disabled={isSubmitting}
-                  id="addon1"
-                  inputProps={{ 'data-testid': 'addon1' } as Partial<NativeSelectInputProps>}
-                  onChange={(event) => setBuild({ ...build, addon1: event.target.value })}
-                  value={build.addon1}
-                >
-                  {addons1.map((name, index) => (
-                    <option key={index} value={name}>
-                      {name}
-                    </option>
-                  ))}
-                </NativeSelect>
-              </FormControl>
-            )}
-            {(buildType === 'killer' || build.item !== 'None') && (
-              <FormControl fullWidth>
-                <InputLabel id="addon2-label">Addon 2</InputLabel>
-                <NativeSelect
-                  aria-labelledby="addon2-label"
-                  disabled={isSubmitting}
-                  id="addon2"
-                  inputProps={{ 'data-testid': 'addon2' } as Partial<NativeSelectInputProps>}
-                  onChange={(event) => setBuild({ ...build, addon2: event.target.value })}
-                  value={build.addon2}
-                >
-                  {addons2.map((name, index) => (
-                    <option key={index} value={name}>
-                      {name}
-                    </option>
-                  ))}
-                </NativeSelect>
-              </FormControl>
-            )}
+            {buildType === 'survivor' &&
+              renderOptionList('Item', 'item', build.item, Object.keys(buildOptions.SurvivorItems))}
+            {(buildType === 'killer' || build.item !== 'None') &&
+              renderOptionList('Addon 1', 'addon1', build.addon1, addons1)}
+            {(buildType === 'killer' || build.item !== 'None') &&
+              renderOptionList('Addon 2', 'addon2', build.addon2, addons2)}
             <Divider />
-            <FormControl fullWidth>
-              <InputLabel id="perk1-label">Perk 1</InputLabel>
-              <NativeSelect
-                aria-labelledby="perk1-label"
-                disabled={isSubmitting}
-                id="perk1"
-                inputProps={{ 'data-testid': 'perk1' } as Partial<NativeSelectInputProps>}
-                onChange={(event) => setBuild({ ...build, perk1: event.target.value })}
-                value={build.perk1}
-              >
-                {perks1.map((name, index) => (
-                  <option key={index} value={name}>
-                    {name}
-                  </option>
-                ))}
-              </NativeSelect>
-            </FormControl>
-            <FormControl fullWidth>
-              <InputLabel id="perk2-label">Perk 2</InputLabel>
-              <NativeSelect
-                aria-labelledby="perk2-label"
-                disabled={isSubmitting}
-                id="perk2"
-                inputProps={{ 'data-testid': 'perk2' } as Partial<NativeSelectInputProps>}
-                onChange={(event) => setBuild({ ...build, perk2: event.target.value })}
-                value={build.perk2}
-              >
-                {perks2.map((name, index) => (
-                  <option key={index} value={name}>
-                    {name}
-                  </option>
-                ))}
-              </NativeSelect>
-            </FormControl>
-            <FormControl fullWidth>
-              <InputLabel id="perk3-label">Perk 3</InputLabel>
-              <NativeSelect
-                aria-labelledby="perk3-label"
-                disabled={isSubmitting}
-                id="perk3"
-                inputProps={{ 'data-testid': 'perk3' } as Partial<NativeSelectInputProps>}
-                onChange={(event) => setBuild({ ...build, perk3: event.target.value })}
-                value={build.perk3}
-              >
-                {perks3.map((name, index) => (
-                  <option key={index} value={name}>
-                    {name}
-                  </option>
-                ))}
-              </NativeSelect>
-            </FormControl>
-            <FormControl fullWidth>
-              <InputLabel id="perk4-label">Perk 4</InputLabel>
-              <NativeSelect
-                aria-labelledby="perk4-label"
-                disabled={isSubmitting}
-                id="perk4"
-                inputProps={{ 'data-testid': 'perk4' } as Partial<NativeSelectInputProps>}
-                onChange={(event) => setBuild({ ...build, perk4: event.target.value })}
-                value={build.perk4}
-              >
-                {perks4.map((name, index) => (
-                  <option key={index} value={name}>
-                    {name}
-                  </option>
-                ))}
-              </NativeSelect>
-            </FormControl>
+            {renderOptionList('Perk 1', 'perk1', build.perk1, perks1)}
+            {renderOptionList('Perk 2', 'perk2', build.perk2, perks2)}
+            {renderOptionList('Perk 3', 'perk3', build.perk3, perks3)}
+            {renderOptionList('Perk 4', 'perk4', build.perk4, perks4)}
             <Divider />
-            <FormControl fullWidth>
-              <InputLabel id="offering-label">Offering</InputLabel>
-              <NativeSelect
-                aria-labelledby="offering-label"
-                disabled={isSubmitting}
-                id="offering"
-                inputProps={{ 'data-testid': 'offering' } as Partial<NativeSelectInputProps>}
-                onChange={(event) => setBuild({ ...build, offering: event.target.value })}
-                value={build.offering}
-              >
-                {offerings.map((name, index) => (
-                  <option key={index} value={name}>
-                    {name}
-                  </option>
-                ))}
-              </NativeSelect>
-            </FormControl>
+            {renderOptionList('Offering', 'offering', build.offering, offerings)}
             <Divider />
             <label>
               <TextField
